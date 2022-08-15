9736ee7c0a6664ac861fc8d34d39318b1fb28b20
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-08-15 23:40:29 +0600

    firebase hosting

--- src/Component/Home/About/About.js
+++ src/Component/Home/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="hero min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
-            <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+            <img src="https://images.livemint.com/img/2021/09/27/1600x900/AFP_9DD4KZ_1629870213363_1632742003316.jpg" className="max-w-sm rounded-lg shadow-2xl" />
             <div>
             <h1 className="text-5xl font-bold">Motion View</h1>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
